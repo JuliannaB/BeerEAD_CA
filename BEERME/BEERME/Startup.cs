27b6927334e295b829d489c6ad7803f8a793b4fa
James <jamesfcadden@gmail.com>
2018-12-06 19:28:31 +0000

    Created BEERME version of MVC app

---
+++ BEERME/BEERME/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BEERME.Startup))]
+namespace BEERME
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
