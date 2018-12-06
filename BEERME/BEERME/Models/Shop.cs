da40e32bb047811a4bd314ac96860b705d095a67
James <jamesfcadden@gmail.com>
2018-12-06 19:33:15 +0000

    Added basic models for Beer, Stock and Shop.

---
+++ BEERME/BEERME/Models/Shop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BEERME.Models
+{
+    public class Shop
+    {
+        [Key]
+        public int ShopID { get; set; }
+        public string ShopName { get; set; }
+        public string ShopAddress { get; set; }
+
+        public virtual ICollection<Stock> Stocks { get; set; }
+    }
+}
---
+++ BEERME/BEERME/Models/Stock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BEERME.Models
+{
+    public class Stock
+    {
+        public int StockID { get; set; }
+
+        public int BeerID { get; set; }
+
+        public int ShopID { get; set; }
+
+        public double Price { get; set; }
+
+        public double PricePerUnit
+        {
+            get
+            {
+                return Math.Pow((((Beer.Volume * (Beer.ABV / 100)) / 10)) / Price, 2);
+            }
+            set
+            {
+                value = PricePerUnit;
+            }
+        }
+
+        public virtual Beer Beer { get; set; }
+        public virtual Shop Shop { get; set; }
+
+    }
+}
